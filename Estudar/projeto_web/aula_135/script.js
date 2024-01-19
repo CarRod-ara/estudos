1bdf7aaf591f802edabc29c1dad017156647ae4d
CarRod-ara <63057416+CarRod-ara@users.noreply.github.com>
2024-01-19 13:29:57 -0300

    19/01

---
+++ Estudar/projeto_web/aula_135/script.js
@@ -0,0 +1,10 @@
+// comentário de js
+console.log(1);
+//console.log(2);
+console.log(3);
+/*este
+comentário
+tem muitas 
+linhas*/
+
+/* rolê aqui de tentar bagulhos*/
---
+++ Estudar/projeto_web/aula_137/script.js
@@ -0,0 +1,16 @@
+var nome = "João Ribeiro"
+var idade = 21
+var administrador = false
+
+console.log(nome)
+
+var cidade
+cidade = "Lisboa"
+
+var valor1 = 100, nome2 = "Ana", apelido = "Silva"
+
+let texto = "frase de um texto"
+const pi = 3.14
+texto = "nada"
+pi = 25
+//teoricamente errado por outro valor na const "pi"
