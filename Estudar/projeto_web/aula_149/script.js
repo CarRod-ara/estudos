436a655929a4815562fc5abb2b1b370aee937f65
CarRod-ara <63057416+CarRod-ara@users.noreply.github.com>
2024-01-23 14:36:13 -0300

    23/01

---
+++ Estudar/projeto_web/aula_149/script.js
@@ -0,0 +1,11 @@
+let nomes = ["joao", "ana", "carlos"]
+
+/*console.table(nomes)
+let outros_nomes = ["joaquim", "carla", "manuel"]
+nomes = nomes.concat(outros_nomes)
+console.table(nomes)*/
+
+/*console.log(nomes.includes("rui"));*/
+
+console.table(nomes)
+console.log(nomes.indexOf("carlos"))
---
+++ Estudar/projeto_web/aula_150/script.js
@@ -0,0 +1,17 @@
+let nomes = ["joão", "ana", "carlos", "xavier", "catarina", "fernanda"];
+
+//por em ordem alfabetica
+/*nomes.sort();
+console.table(nomes);*/
+
+//por em ordem decrescente alfabetica
+/*nomes.sort();
+nomes.reverse();
+console.table(nomes);*/
+
+//mesmo exemplo anterior, contudo mais compacto
+/*nomes.sort().reverse();
+console.table(nomes);*/
+
+//aqui já virou p******, mas também da pra fazer dessa forma
+console.table(nomes.sort().reverse());
