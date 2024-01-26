8ebb49b1b7e36e26f6afc786e5bfe66703738c72
Carlos Eduardo <carloseduardo.arajo@gmail.com>
2024-01-26 13:45:50 -0300

    26/015

--- Estudar/projeto_web/aula_155/script.js
+++ Estudar/projeto_web/aula_155/script.js
@@ -3,7 +3,24 @@
 let pessoa = {
 
     // propriedades
-    nome:"Joao"
-    sobrenome: "Ribeiro"
+    nome:"Joao",
+    sobrenome: "Ribeiro",
+    idade: 48,
+    genero: "masculino",
+
+    //métodos
+    apresentar_nome: function() {
+        return this.nome + " " + this.apelido
+    },
+
+    apresentar_idade: function() {
+        return this.idade + " anos de idade"
+    },
+
+    hobbies: [
+        'Programação',
+        'Cinema',
+        'Música'
+    ]
 
 }
