c78618b5f6d078781547661fa599a6d1b401e8c6
Carlos Eduardo <carloseduardo.arajo@gmail.com>
2024-02-04 15:17:42 -0300

    0402

--- Estudar/projeto_web/aula_162/script.js
+++ Estudar/projeto_web/aula_162/script.js
@@ -1,1 +1,8 @@
-document.querySelector("h1").style.color = "red";
+//document.querySelector("h1").style.color = "red";
+
+//console.log(el.style.backgroundColor)
+//let el = document.querySelector("p")
+
+let el = document.querySelector("p")
+const estilos = window.getComputedStyle(el)
+console.log(estilos.getPropertyValue('font-size'))
