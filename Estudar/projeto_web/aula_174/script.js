17f0a15c95292aadf0faf736edd073df2d967edd
Carlos Eduardo <carloseduardo.arajo@gmail.com>
2024-02-23 15:06:02 -0300

    23/02
    erro no javascript

---
+++ Estudar/projeto_web/aula_174/script.js
@@ -0,0 +1,47 @@
+// INTRODUÇÃO À VALIDAÇÃO DE FORMULÁRIOS
+
+//usuario
+document.querySelector('input[name="usuario"]').addEventListener('keyup', () => {
+    console.log(document.querySelector('input[name="usuario"] + span'))
+})
+
+document.formulario_login.addEventListener('submit', (event) => {
+
+    //buscar valores dos campos
+    let usuario = event.target.usuario.value
+    let senha = event.target.senha.value
+    let submit = true
+
+    //remover erros anteriores
+    document.querySelector('.error').forEach( e => e.remove())
+
+    //validar usuario
+    if(usuario === ""){
+        submit = false
+
+        //erro
+        let tmp = document.querySelector('input[name="usuario"]')
+        tmp.insertAdjacentHTML('afterend','<span class="error">Usuário é obrigatório.')
+    }
+
+    //validar senha
+    if(senha === ""){
+        submit = false
+
+        //erro
+        let tmp = document.querySelector('input[name="senha"]')
+        tmp.insertAdjacentHTML('afterend','<span class="error">Senha é obrigatória.')
+
+    }
+
+    //verificar se o formulário pode ser enviado
+    if(!submit){
+        event.preventDefault()
+
+        //faz com que os erros sumam após dois segundos
+
+        /*setTimeout(() => {
+            document.querySelectorAll(".error").forEach(e => e.remove())
+        }, 2000)*/
+    }
+})
