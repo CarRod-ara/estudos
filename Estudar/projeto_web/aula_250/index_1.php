cef5f176f1e203257749aa10b6becb8afd6ea281
Carlos Eduardo <carloseduardo.arajo@gmail.com>
2024-04-03 22:42:39 -0300

    03/04

---
+++ Estudar/projeto_web/aula_250/index_1.php
@@ -0,0 +1,21 @@
+<?php
+
+// linha única de comentário
+
+# Também linha única de comentário
+
+/* concatinação de linhas
+Até a realização do fechamento */
+
+// --------------------------------
+// Comentários podem entrar aqui
+// --------------------------------
+
+#------------------------------
+# Comentários podem entrar aqui
+#------------------------------
+
+/* ----------------------------
+Comentarários podem entrar aqui
+E nesta segunda linha também
+----------------------------*/
